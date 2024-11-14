386cdc5247354af8c087fcc02b633605e9d87776
Matias <matiassanchez13422@gmail.com>
2024-11-14 17:57:42 -0300

    Se corrigio los valores de let y const

--- js/app.js
+++ js/app.js
@@ -1,14 +1,13 @@
-// 5- Realizar una web con un cronómetro, que tenga las opciones de iniciar, reset (volver el cronómetro a 0) y pausar.
 let segundos = 0;
-let estaPausado = false
+let estaPausado = false;
 let intervalo; 
 
 function IniciarCronometro() {
     console.log('desde IniciarCronometro');
     const cronometro = document.querySelector('p'); 
-    console.log(cronometro)
+    console.log(cronometro);
 
-//    Agregar condicion logica
+    // Agregar condicion logica
     if (BotonIniciar.innerHTML === 'Reiniciar') {
         // ClearInterval detiene el Intervalo
         clearInterval(intervalo); 
@@ -17,21 +16,18 @@ function IniciarCronometro() {
         BotonIniciar.innerHTML = 'Iniciar'; 
         return; 
     }
- 
-    
-    
-   
+
     BotonIniciar.innerHTML = 'Reiniciar'; 
 
-    
     intervalo = setInterval(() => {
         segundos++;
         let minutos = Math.floor(segundos / 60);
         let segundosRestantes = segundos % 60;
 
         // Mostrar los minutos
-        // Agregamos condicion logica (var < 10 ? 'o' : '') para que el resto de la division mientras sea menor a 10 se agregue un cero adelante de los segundos 
-        cronometro.innerHTML = minutos + ':' + (segundosRestantes < 10 ? '0' : '') + segundosRestantes;}, 1000); 
+        // Agregamos condicion logica (var < 10 ? '0' : '') para que el resto de la division mientras sea menor a 10 se agregue un cero adelante de los segundos 
+        cronometro.innerHTML = minutos + ':' + (segundosRestantes < 10 ? '0' : '') + segundosRestantes;
+    }, 1000); 
 }
 
 function PausarCronometro() {
@@ -39,17 +35,14 @@ function PausarCronometro() {
     const BotonIniciar = document.querySelector('.btn-primary');
     const BotonPausar = document.querySelector('.btn-secondary'); 
 
-    
     if (estaPausado) {
-        
-    //true = Si el cronómetro está pausado lo despausamos siguiendo con el setInterval
+        // true = Si el cronómetro está pausado lo despausamos siguiendo con el setInterval
         intervalo = setInterval(() => {
             segundos++; 
 
             let minutos = Math.floor(segundos / 60);
             let segundosRestantes = segundos % 60;
 
-           
             cronometro.innerHTML = minutos + ':' + (segundosRestantes < 10 ? '0' : '') + segundosRestantes;
         }, 1000);
 
@@ -57,7 +50,7 @@ function PausarCronometro() {
         BotonIniciar.innerHTML = 'Reiniciar'; 
         estaPausado = false; 
     } else {
-        //false = Pausar cronometro con el clearInterval
+        // false = Pausar cronometro con el clearInterval
         clearInterval(intervalo); 
         BotonPausar.innerHTML = 'Reanudar'; 
         estaPausado = true;
@@ -65,11 +58,7 @@ function PausarCronometro() {
 }
 
 
-
-
-const BotonIniciar = document.querySelector('.btn-primary')
-BotonIniciar.addEventListener('click ', IniciarCronometro)
-
-
-const BotonPausar = document.querySelector('.btn-secondary')
-BotonPausar.addEventListener('click', PausarCronometro)
+const BotonIniciar = document.querySelector('.btn-primary');
+BotonIniciar.addEventListener('click', IniciarCronometro); 
+const BotonPausar = document.querySelector('.btn-secondary');
+BotonPausar.addEventListener('click', PausarCronometro);
